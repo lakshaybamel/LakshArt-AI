2f8155c2f0bc6b4aab13d313b8cca4c7780f9247
lakshaybamel <lakbamel@gmail.com>
2026-05-23 12:20:01 +0530

    set Flask app port to 5000

--- app.py
+++ app.py
@@ -244,6 +244,4 @@ def send_example(filename):
 # **********RUN**********
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
-
-    app.run(host="0.0.0.0", port=port)
+        app.run(host="0.0.0.0", port=5000)
